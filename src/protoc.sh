f145029ac4339664fb66b22694a4f1cba38c3a2f
kaioduarte <duartedossantos@gmail.com>
2021-08-03 10:58:44 +0100

    publish

--- src/protoc.sh
+++ src/protoc.sh
@@ -6,32 +6,43 @@ cd "${BASEDIR}"/protos
 PROTOC_GEN_TS_PATH="../../node_modules/.bin/protoc-gen-ts"
 GRPC_TOOLS_NODE_PROTOC_PLUGIN="../../node_modules/.bin/grpc_tools_node_protoc_plugin"
 GRPC_TOOLS_NODE_PROTOC="../../node_modules/.bin/grpc_tools_node_protoc"
-OUTPUT_DIR=../../lib
 
-if [ ! -d $OUTPUT_DIR ]; then
-  mkdir $OUTPUT_DIR
-else
-  rm -rf $OUTPUT_DIR
-  mkdir $OUTPUT_DIR
-fi
+PROTO_OUTPUT_DIR="../../protos"
+GENERATED_OUTPUT_DIR="../../generated"
+
+OutputDirPaths=($PROTO_OUTPUT_DIR  $GENERATED_OUTPUT_DIR)
+ 
+# Create output directories
+for path in ${OutputDirPaths[*]}; do
+  if [ ! -d $path ]; then
+    mkdir $path
+  else
+    rm -rf $path
+    mkdir $path
+  fi
+done
 
 for filename in ./*; do
+  # Ignore non proto files
   if [[ "$filename" != *.proto ]]; then
     continue;
   fi
 
+  # Copy proto file to /protos
+  cp $filename $PROTO_OUTPUT_DIR/$filename
+
   # loop over all the available proto files and compile them into respective dir
   # JavaScript code generating
   ${GRPC_TOOLS_NODE_PROTOC} \
-    --js_out=import_style=commonjs,binary:$OUTPUT_DIR \
-    --ts_out=grpc_js:$OUTPUT_DIR \
-    --grpc_out=grpc_js:$OUTPUT_DIR \
+    --js_out=import_style=commonjs,binary:$GENERATED_OUTPUT_DIR \
+    --ts_out=grpc_js:$GENERATED_OUTPUT_DIR \
+    --grpc_out=grpc_js:$GENERATED_OUTPUT_DIR \
     --plugin=protoc-gen-grpc="${GRPC_TOOLS_NODE_PROTOC_PLUGIN}" \
     "${filename}"
 
   ${GRPC_TOOLS_NODE_PROTOC} \
     --plugin=protoc-gen-ts="${PROTOC_GEN_TS_PATH}" \
-    --ts_out=grpc_js:$OUTPUT_DIR \
+    --ts_out=grpc_js:$GENERATED_OUTPUT_DIR \
     "${filename}"
 
 done
